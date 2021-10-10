eefcf3e9bb16351f2f7e4fbbfd71b68f48505f59
Mikhail <75127099+itease@users.noreply.github.com>
2021-10-10 12:31:34 +0300

    Create A.cpp

---
+++ Version_2.0/Division_B/Homework_1/A.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main() {
+    int r, i, c, result;
+    std::cin >> r >> i >> c;
+
+    switch (i) {
+        case 0:
+            if (r != 0) {
+                result = 3;
+            } else {
+                result = c;
+            }
+            break;
+        case 1:
+            result = c;
+            break;
+        case 4:
+            if (r != 0) {
+                result = 3;
+            } else {
+                result = 4;
+            }
+            break;
+        case 6:
+            result = 0;
+            break;
+        case 7:
+            result = 1;
+            break;
+        default:
+            result = i;
+            break;
+    }
+
+    std::cout << result;
+
+    return 0;
+}
